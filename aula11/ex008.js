a779033ddbad6789af6d2673e418e07aedac3e5d
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-01-14 14:28:31 -0300

    incio do estudo com condicoes

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = '80km/h'
+console.log('O console funcionou corretamente')
+
+if(vel > 60){//condicao simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log(`A sua velocidade é ${vel}`)
